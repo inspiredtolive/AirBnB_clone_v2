cf26938aad01c820b48ceb79380d0ce10b4cbce3
Marco Chan <inspiredtolive0@gmail.com>
2020-01-22 16:28:16 -0800

    Fixes a bug where Amenities wasn't being reloaded

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -42,7 +42,7 @@ class DBStorage:
         queried_objs = {}
         if cls is None:
             objs = []
-            classes = [State, City, User, Place, Review]
+            classes = [State, City, User, Place, Review, Amenity]
             for c in classes:
                 results = self.__session.query(c).all()
                 objs.extend(results)
